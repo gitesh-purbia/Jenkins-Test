beb430ced08a405b58bed5ccfdfd1114732440fa
Gitesh <gitesh.purbiya22@gmail.com>
2015-11-06 12:20:38 +0530

    Added .cool routes in app for test

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var path = require('path');
+var cool = require('cool-ascii-faces');
 var app = express();
 
 app.set('port', (process.env.PORT || 3000));
@@ -9,6 +10,10 @@ app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
    res.render('index', { title: 'Express.js Todo App' });
+});
+
+app.get('/cool', function(request, response) {
+  response.send(cool());
 });
 
 app.listen(app.get('port'), function() {
