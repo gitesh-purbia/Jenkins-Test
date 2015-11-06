2810770dd8e4ac6b0cb2f570569671285be1ac0b
Gitesh <gitesh.purbiya22@gmail.com>
2015-11-06 20:58:42 +0530

    add a sample app for testing

---
+++ public/app/app.js
@@ -0,0 +1,5 @@
+var myApp = angular.module('myApp', []);
+myApp.controller('MyCtrl', function(){
+	self = this;
+	self.name = '';
+});
--- server.js
+++ server.js
@@ -8,6 +8,8 @@ app.set('port', (process.env.PORT || 3000));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(express.static('public'));
+
 app.get('/', function (req, res) {
    res.render('index', { title: 'Express.js Todo App' });
 });
