32df87abb1051ae466b4493d5f9f033b70299e46
Gitesh <gitesh.purbiya22@gmail.com>
2015-11-06 19:22:45 +0530

    fix error in mocha sample test file

--- test/sample-test.js
+++ test/sample-test.js
@@ -2,7 +2,7 @@ var assert = require('assert');
 
 describe('thats a sample test', function(){
 	it('should say Hello world', function(){
-		assert.equal('Helloo world', test());
+		assert.equal('Hello world', test());
 	});
 });
 
