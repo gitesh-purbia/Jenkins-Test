56590443b34092fead356501fd9f709a211ca55c
Gitesh <gitesh.purbiya22@gmail.com>
2015-11-09 14:29:10 +0530

    add e2e testing for demo with protractor

--- e2e/example-spec.js
+++ e2e/example-spec.js
@@ -1,8 +1,14 @@
+var chai = require('chai');
+var chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+var expect = chai.expect;
+
 describe('cheking model', function(){
 	it('should print value by reading textbox', function(){
+		this.timeout(9000);
 		browser.get('http://localhost:3000');
 		element(by.model('ctrl.name')).sendKeys('write first protractor test');
 		var message = element(by.css('.message'));
-		expect(message.getText()).toEqual('write first protractor test');
+		expect(message.getText()).to.eventually.equal('write first protractor test');
 	});
 });
--- protractor-conf.js
+++ protractor-conf.js
@@ -1,18 +1,10 @@
 exports.config = {
   
+  framework: 'mocha',
   seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
   
   capabilities: {
     'browserName': 'chrome',
-     "chromeOptions": {
-        binary: "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe",
-        args: [],
-        extensions: [],
-    }
-    
-  },
-
-  jasmineNodeOpts: {
-    showColors: true
   }
+  
 };
